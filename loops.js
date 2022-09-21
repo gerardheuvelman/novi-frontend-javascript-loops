0ff3e69e130574a38a1a98e71678618fae269eaf
gerardheuvelman <gerardheuvelman@gmail.com>
2022-09-21 15:24:51 +0200

    alles is af

---
+++ loops.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+let j = 0;
+do {
+    console.log(j);
+    j++;
+    } while (j < 10);
